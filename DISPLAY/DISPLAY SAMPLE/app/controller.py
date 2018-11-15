05079732cf063745443aa76939569e4ed42064e0
Bleidd <glen.escuadra@g.msuiit.edu.ph>
2018-11-15 15:34:18 +0800

    removed unnecessary files

--- DISPLAY/DISPLAY SAMPLE/app/controller.py
+++
@@ -1,9 +0,0 @@
-from app import app
-import models
-from flask import render_template, redirect, request
-
-
-@app.route('/')
-def index():
-    users = models.Users.all()
-    return render_template('index.html', data=users)
